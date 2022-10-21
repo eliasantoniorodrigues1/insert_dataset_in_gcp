0559b098eee37bd76737461158e9e6aebdae4ddd
Elias <eliasantoniorodrigues1@gmail.com>
2022-10-21 18:01:35 -0300

    Fianlizacao da insercao do dataset em uma tabela do GCP

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from google.cloud import bigquery
 import pandas as pd
 import sys
 import os
+import re
 
-from gbq_dataset import PROJECT_NAME
+sys.path.append('..')
+
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 sys.path.append('..')
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join(BASE_DIR, 'credentials/gcp_credentials.json')
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join(
+    BASE_DIR, 'credentials/gcp_credentials.json')
+
 
 def create_gcp_dataset(client):
     dataset_id = f'{client.project}.scraper_query_price'
@@ -16,16 +20,57 @@ def create_gcp_dataset(client):
 
     dataset.location = 'US'
 
-    dataset = client.create_dataset(dataset, timeout=30) # make an API request
+    dataset = client.create_dataset(dataset, timeout=30)  # make an API request
     return f'Created a dataset {client.project}.{dataset.dataset_id}'
 
 
+def create_bigquery_table(df, dataset_tablename, gcp_project_name):
+    # df: your pandas dataframe
+    # dataset_tablename (str.str): dataset_name.tablename
+    # gcp_project_name: GCP Project ID
 
+    df.to_gbq(
+        destination_table=dataset_tablename,
+        project_id=gcp_project_name,
+        if_exists="replace",  # 3 available methods: fail/replace/append
+        progressbar=True
+    )
+
+
+def remove_char(text: str):
+    try:
+        new_text = re.sub(r'[:\s0-9]+', '_', text)
+        return new_text
+    except:
+        return text
 
 if __name__ == '__main__':
-    project_name = 'study-gbq-ear'
-    client = bigquery.Client(project=project_name)
-    
-    # dataset = pd.read_csv('quered_car.csv')
-    
+    # project definitions
+    project_id = 'study-gbq-ear'
+    dataset_id = 'scraper_query_price'
+    table_id = 'scraped_cars'
 
+    # object creation
+    client = bigquery.Client(project=project_id)
+
+    # msg = create_gcp_dataset(client)
+
+    # generate data
+    dataset = pd.read_csv('consolidate.csv', encoding='utf-8')
+    columns = dataset.columns
+    
+    new_columns = []
+    for column in columns:
+        c = remove_char(column)
+        new_columns.append(c)    
+    
+    dataset.columns.values[:] = new_columns
+
+    dataset.to_gbq(
+                destination_table=f'{project_id}.{dataset_id}.{table_id}', 
+                project_id=project_id, 
+                chunksize=None,
+                if_exists='append',
+                progress_bar=True
+    )
+    print(f'Dados inseridos com sucesso em {project_id}.{dataset_id}.{table_id}')
