b446094284907d48275baa7ebe42bcd29f4a2beb
Elias Rodrigues <49626719+eliasantoniorodrigues1@users.noreply.github.com>
2022-10-21 18:04:06 -0300

    Delete auth.py

--- auth.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-sys.path.append('..')
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join(BASE_DIR, 'credentials/gcp_credentials.json')
